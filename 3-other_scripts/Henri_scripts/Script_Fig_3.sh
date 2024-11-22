3c29d5f9921110ab0c59fe0f75dc16e5fa41c775
hsiljanen <159240825+hsiljanen@users.noreply.github.com>
2024-11-22 06:27:36 +0200

    Update Script_Fig_3.sh
    Chnages after PCI-Microbiology revision for Fig. 3. Previous versions are below.

--- 3-other_scripts/Henri_scripts/Script_Fig_3.sh
+++ 3-other_scripts/Henri_scripts/Script_Fig_3.sh
@@ -3,7 +3,195 @@
 #  Script_Fig_3.sh
 #  
 #
-#  Created by Henri Siljanen2 on 9.2.2024.
+# Created by Henri Siljanen on 28.9.2024, after pci-microbiology revision
+
+
+setwd("/Users/siljanen/Library/Mobile\ Documents/com\~apple\~CloudDocs/Documents/Probe_capture2_MG-documents/MetCap-N_1st_Validation_Outcome_data+results+Manuscript/")
+
+#superpose function
+superpose.eb <-
+function (x, y, ebl, ebu = ebl, length = 0.04, ...)
+    arrows(x, y + ebu, x, y - ebl, angle = 90, code = 3,
+    length = length, ...)
+
+#Belfontaine:
+Total_read_RA <- matrix(c(
+0.001911033, 1.54089E-05, 0.000102607, 0.002340381, 0.000821016, 7.64167E-07, 0.00244101, 0.003668652, 0.000292022, 0.001648217, 0.001723354, 0.001398577, 0.000128771, 0.000103205, 0.000689391, 
+13.34051243, 0.139450074, 0.733383736, 2.59240322, 0.58008041, 0.00130378, 10.03299, 9.35380698, 1.454513908, 10.255586, 1.070992322, 4.059711994, 0.981120149, 0.171831185, 1.898530578
+),nrow = 2,ncol = 15,byrow = TRUE)
+
+Values_sd <- matrix(c(
+0.000997664, 1.15718E-05, 3.02979E-05, 0.000274423, 0.000346357, 1.0807E-06, 0.000335242, 0.001070409, 3.16418E-05, 0.000621598, 0.000404019, 9.31457E-05, 2.22262E-05, 8.52551E-05, 0.000711681, 
+3.949794036, 0.078180128, 0.49125469, 2.894437223, 0.123749205, 0.001029063, 2.504484603, 1.117919135, 0.323352616, 0.327699814, 0.355213166, 1.107116742, 0.176496915, 0.061601251, 1.526503534
+),nrow = 2,ncol = 15,byrow = TRUE)
+
+colnames(Total_read_RA ) = c("nifH",	"TamoA",	"amoA",	"nxrB",	"nrfA",	"hzoA",	"napA",	"narG",	"nirK",	"nirS",	"norB",	"nosZ",	"pmoA",	"mmoX",	"mcrA")
+rownames(Total_read_RA ) = c("Shotgun ", "Captured  ")
+
+setEPS()
+postscript("belfointaine_N_reads_fig3pci.eps")  # create eps -file
+#png(file = "belfointaine_N_reads_fig3pci.png")   # Give the chart file a name.
+belfontaine_fig3 <- barplot(Total_read_RA , beside=TRUE,  
+ ylim=c(0,20), main="Belfontaine",
+    xlab="Genes", ylab = "Relative abundance, total reads (%)", legend.text = c("Shotgun metagenomics", "Captured metagenomics")
+)
+dev.off()  # Save the file.
+
+setEPS()
+postscript("belfointaine_N_reads_fig3pci.eps")  # create eps -file
+#png(file = "belfointaine_N_reads_fig3pci.png")   # Give the chart file a name.
+belfontaine_fig3 <- barplot(Total_read_RA, beside=TRUE,  
+ ylim=c(-1.0,30.0), main="Belfontaine",
+    xlab="Genes", ylab = "Relative abundance, total reads (%)", legend.text = c("Shotgun metagenomics", "Captured metagenomics"),
+    args.legend = list(x = "topleft"))
+
+superpose.eb(belfontaine_fig3, Total_read_RA , Values_sd, col="black", lwd=1.5)
+dev.off()  # Save the file.
+
+
+setEPS()
+postscript("belfointaine_N_reads_fig3pci_s.eps")  # create eps -file
+#png(file = "belfointaine_N_reads_fig3pci.png")   # Give the chart file a name.
+belfontaine_fig3_s <- barplot(Total_read_RA, beside=TRUE,  
+ ylim=c(-0.005,0.005), main="Belfontaine",
+    xlab="Genes", ylab = "Relative abundance, total reads (%)")
+abline(h=0)
+
+superpose.eb(belfontaine_fig3_s, Total_read_RA , Values_sd, col="black", lwd=1.5)
+dev.off()  # Save the file.
+
+
+
+
+
+#Hungarian:
+Total_read_RA_H <- matrix(c(
+7.34066E-05, 0.000292496, 3.11156E-05, 0.00057338, 0.000603068, 0, 0.002462011, 0.002462252, 0.000829175, 0.00010485, 0.000442137, 0.001355456, 3.00713E-05, 0.000151054, 0,
+
+1.042072243, 2.611045776, 0.293029832, 0.580382904, 0.811505869, 0, 19.88344074, 9.456528627, 5.273714839, 0.62998497, 2.714774935, 4.857191124, 0.060250088, 4.54600597, 0.000213994
+
+),nrow = 2,ncol = 15,byrow = TRUE)
+
+Values_sd_H <- matrix(c(
+6.30724E-05, 0.000239411, 2.31809E-05, 0.000236922, 0.000258183, 0, 0.001115166, 0.001131136, 0.000356943, 5.68066E-05, 0.000197648, 0.000644432, 1.70702E-05, 0.000107277, 0,
+
+0.065579758, 0.460207367, 0.082249559, 0.02696476, 0.006875067, 0, 0.295429452, 0.049540031, 0.145910308, 0.064743462, 0.079618242, 0.272345285, 0.004760226, 0.483157914, 0.000302633
+),nrow = 2,ncol = 15,byrow = TRUE)
+
+colnames(Total_read_RA_H) = c("nifH",	"TamoA",	"amoA",	"nxrB",	"nrfA",	"hzoA",	"napA",	"narG",	"nirK",	"nirS",	"norB",	"nosZ",	"pmoA",	"mmoX",	"mcrA")
+rownames(Total_read_RA_H) = c("Shotgun ", "Captured  ")
+
+setEPS()
+postscript("hungarian_RA_reads_fig3pci.eps")  # create eps -file
+#png(file = "hungarian_RA_reads_fig3pci.png")   # Give the chart file a name.
+hungarian_fig3 <- barplot(Total_read_RA_H, beside=TRUE,  
+    ylim=c(-1.0,25.0), main="Hungarian",
+    xlab="Genes", ylab = "Relative abundance, total reads (%)", legend.text = c("Shotgun metagenomics", "Captured metagenomics"),
+    args.legend = list(x = "topleft"))
+
+superpose.eb(hungarian_fig3, Total_read_RA_H, Values_sd_H, col="black", lwd=1.5)
+dev.off()  # Save the file.
+
+setEPS()
+postscript("hungarian_RA_reads_fig3pci_s.eps")  # create eps -file
+#png(file = "hungarian_RA_reads_fig3pci_s.png")   # Give the chart file a name.
+hungarian_fig3s <- barplot(Total_read_RA_H, beside=TRUE,  
+    ylim=c(-0.005,0.005), main="Hungarian",
+    xlab="Genes", ylab = "Relative abundance, total reads (%)" )
+abline(h=0)
+
+superpose.eb(hungarian_fig3s, Total_read_RA_H, Values_sd_H, col="black", lwd=1.5)
+dev.off()  # Save the file.
+
+
+#Fig. 3_Revision_after_PCI_microbiology
+
+
+getwd()
+
+setwd("/Users/siljanen/Library/Mobile Documents/com~apple~CloudDocs/Documents/Probe_capture2_MG-documents/MetCap-N_1st_Validation_Outcome_data+results+Manuscript")
+
+#Belfontaine
+Total_reads_RA_shotgun_captured_belfontaine <-read.csv("total_reads_RA_shotgun_captured_belfontaine.csv",header=T, sep=";")
+##ANOVA + tukeyHSD for number of reads with capture or shotgun:
+
+Total_reads_RA_shotgun_captured_belfontaine.aov <- aov(Total_read_RA~factor(Method)*factor(Gene), Total_reads_RA_shotgun_captured_belfontaine)
+summary(Total_reads_RA_shotgun_captured_belfontaine.aov)
+
+         Df Sum Sq Mean Sq F value   Pr(>F)    
+factor(Method)               1  320.9   320.9  179.82  < 2e-16 ***
+factor(Gene)                14  433.9    31.0   17.37 4.27e-16 ***
+factor(Method):factor(Gene) 14  433.6    31.0   17.36 4.34e-16 ***
+Residuals                   60  107.1     1.8                     
+---
+Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
+> 
+
+
+TukeyHSD(Total_reads_RA_shotgun_captured_belfontaine.aov, ordered=FALSE)
+
+$`factor(Method):factor(Gene)`
+                                      diff         lwr        upr     p adj
+shotgun:TamoA-captured:TamoA -1.394347e-01  -4.4326442  4.1537749 1.0000000
+shotgun:amoA-captured:amoA   -7.332811e-01  -5.0264907  3.5599284 1.0000000
+shotgun:hzoA-captured:hzoA   -1.303016e-03  -4.2945126  4.2919065 1.0000000
+shotgun:mcrA-captured:mcrA   -1.897841e+00  -6.1910507  2.3953684 0.9945745
+shotgun:mmoX-captured:mmoX   -1.717280e-01  -4.4649375  4.1214816 1.0000000
+shotgun:napA-captured:napA   -1.003055e+01 -14.3237585 -5.7373395 0.0000000
+shotgun:narG-captured:narG   -9.350138e+00 -13.6433479 -5.0569288 0.0000000
+shotgun:nifH-captured:nifH   -1.333860e+01 -17.6318109 -9.0453919 0.0000000
+shotgun:nirK-captured:nirK   -1.454222e+00  -5.7474314  2.8389877 0.9999322
+shotgun:nirS-captured:nirS   -1.025394e+01 -14.5471473 -5.9607282 0.0000000
+shotgun:norB-captured:norB   -1.069269e+00  -5.3624785  3.2239406 0.9999999
+shotgun:nosZ-captured:nosZ   -4.058313e+00  -8.3515230  0.2348961 0.0888384
+shotgun:nrfA-captured:nrfA   -5.792594e-01  -4.8724689  3.7139501 1.0000000
+shotgun:nxrB-captured:nxrB   -2.590063e+00  -6.8832724  1.7031467 0.8382351
+shotgun:pmoA-captured:pmoA   -9.809914e-01  -5.2742009  3.3122182 1.0000000
+
+
+
+
+
+#Hungary
+Total_reads_RA_shotgun_captured_hungary <-read.csv("total_reads_RA_shotgun_captured_hungary.csv",header=T, sep=";")
+##ANOVA + tukeyHSD for number of reads with capture or shotgun:
+
+Total_reads_RA_shotgun_captured_hungary.aov <- aov(number_reads~factor(Method)*factor(Gene), Total_reads_RA_shotgun_captured_hungary)
+summary(Total_reads_RA_shotgun_captured_hungary.aov)
+
+                    Df Sum Sq Mean Sq F value Pr(>F)    
+factor(Method)               1  278.3  278.26    8525 <2e-16 ***
+factor(Gene)                14  582.2   41.59    1274 <2e-16 ***
+factor(Method):factor(Gene) 14  581.9   41.56    1273 <2e-16 ***
+Residuals                   60    2.0    0.03                   
+---
+Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
+
+
+TukeyHSD(Total_reads_RA_shotgun_captured_hungary.aov, ordered=FALSE) 
+
+$`factor(Method):factor(Gene)`
+                                      diff           lwr           upr     p adj
+shotgun:TamoA-captured:TamoA -2.610753e+00 -3.191372e+00 -2.030134e+00 0.0000000
+shotgun:amoA-captured:amoA   -2.929987e-01 -8.736176e-01  2.876202e-01 0.9707278
+shotgun:hzoA-captured:hzoA    6.217249e-15 -5.806189e-01  5.806189e-01 1.0000000
+shotgun:mcrA-captured:mcrA   -2.139940e-04 -5.808329e-01  5.804049e-01 1.0000000
+shotgun:mmoX-captured:mmoX   -4.545855e+00 -5.126474e+00 -3.965236e+00 0.0000000
+shotgun:napA-captured:napA   -1.988098e+01 -2.046160e+01 -1.930036e+01 0.0000000
+shotgun:narG-captured:narG   -9.454066e+00 -1.003469e+01 -8.873448e+00 0.0000000
+shotgun:nifH-captured:nifH   -1.041999e+00 -1.622618e+00 -4.613800e-01 0.0000008
+shotgun:nirK-captured:nirK   -5.272886e+00 -5.853505e+00 -4.692267e+00 0.0000000
+shotgun:nirS-captured:nirS   -6.298801e-01 -1.210499e+00 -4.926125e-02 0.0189298
+shotgun:norB-captured:norB   -2.714333e+00 -3.294952e+00 -2.133714e+00 0.0000000
+shotgun:nosZ-captured:nosZ   -4.855836e+00 -5.436455e+00 -4.275217e+00 0.0000000
+shotgun:nrfA-captured:nrfA   -8.109028e-01 -1.391522e+00 -2.302839e-01 0.0003009
+shotgun:nxrB-captured:nxrB   -5.798095e-01 -1.160428e+00  8.093489e-04 0.0507650
+shotgun:pmoA-captured:pmoA   -6.022002e-02 -6.408389e-01  5.203989e-01 1.0000000
+
+
+
+
+#  Created by Henri Siljanen2 on 9.2.2024. (earlier version)
 #  
 
 #superpose function
